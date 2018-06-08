e5947cdcdca96c462b3b6ece9103225b8acf5644
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-06-08 22:41:13 +1200

    try to implement screenshot

--- public/index.js
+++ public/index.js
@@ -1,13 +1,26 @@
 let arr = [];
-for (let i = 0; i < 7; i++){
-    let image = "figure" + (i+1).toString();
-    let marker = "marker" + (i+1).toString();
-    arr.push({image:image,marker:marker})
+for (let i = 0; i < 7; i++) {
+	let image = "figure" + (i + 1).toString();
+	let marker = "marker" + (i + 1).toString();
+	arr.push({ image: image, marker: marker });
 }
 
 var app = new Vue({
-    el: '#app',
-    data: {
-      markerData: arr
-    }
-  })
+	el: "#app",
+	data: {
+		markerData: arr
+	}
+});
+
+document.getElementById("snap-button").addEventListener("click", function() {
+	this.parentNode.style.display = "none";
+	html2canvas(document.querySelector("body")).then(canvas => {
+		this.parentNode.style.display = "";
+		document.body.appendChild(canvas);
+		var image = canvas
+			.toDataURL("image/png")
+			.replace("image/png", "image/octet-stream"); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+
+		window.location.href = image; // it will save locally
+	});
+});
