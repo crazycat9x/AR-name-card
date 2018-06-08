fdceeac5cbea39f206ebefbf1c473f0735ff106d
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-06-08 16:07:11 +1200

    fix bug where counter exceed actusl markers

--- public/index.js
+++ public/index.js
@@ -1,11 +1,9 @@
 let arr = [];
-for (let i = 0; i < 8; i++){
+for (let i = 0; i < 7; i++){
     let image = "figure" + (i+1).toString();
     let marker = "marker" + (i+1).toString();
     arr.push({image:image,marker:marker})
 }
-
-console.log(arr)
 
 var app = new Vue({
     el: '#app',
